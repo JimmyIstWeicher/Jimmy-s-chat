e74d8ca892c7d2506f87457974044b57685fd015
JimmyIstWeicher <emil.der.teddybaer@gmail.com>
2023-12-16 19:13:31 +0100

    Name geändert

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'JiChat',
+  title: 'JimmyChat',
   description: 'Sprich mit Jimmy',
   manifest: "/manifest.json",
   icons: {apple: "/icon.png"},
